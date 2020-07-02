d4b65ce1a34bb22fd39055a485b395a7115eb89c
James <51536154+zukyoo@users.noreply.github.com>
2020-07-02 18:52:03 +0100

    Update register.go

--- register.go
+++ register.go
@@ -111,8 +111,8 @@ func registerSubmit(c *gin.Context) {
 		return
 	}
 
-	res, err := db.Exec(`INSERT INTO users(username, username_safe, password_md5, salt, email, register_datetime, privileges, password_version)
-							  VALUES (?,        ?,             ?,            '',   ?,     ?,                 ?,          2);`,
+	res, err := db.Exec(`INSERT INTO users(username, username_safe, password_md5, salt, email, register_datetime, privileges, password_version, frozen, freezedate)
+							  VALUES (?,        ?,             ?,            '',   ?,     ?,                 ?,          2,          0,          0);`,
 		username, safeUsername(username), pass, c.PostForm("email"), time.Now().Unix(), common.UserPrivilegePendingVerification)
 	if err != nil {
 		registerResp(c, errorMessage{T(c, "Whoops, an error slipped in. You might have been registered, though. I don't know.")})
